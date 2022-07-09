90b747a95303ef97eefef9659b60c7f7699698b6
Adam-Teng <656081024@qq.com>
2022-07-09 21:57:37 +0800

    finish disc02

---
+++ DISC/disc02/q7.py
@@ -0,0 +1,8 @@
+def match_k_alt(k):
+    def check(x):
+        while x // (10 ** k):
+            if (x % 10) != (x // (10 ** k)) % 10:
+                return False
+            x //= 10
+        return True
+    return check
