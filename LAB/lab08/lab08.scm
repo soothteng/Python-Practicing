7d2cfae80fd0feb912dd5b84171cda6e833e1738
Adam-Teng <656081024@qq.com>
2022-08-01 07:24:27 +0800

    add disc09 disc10  lab09 lab08-sol

--- LAB/lab08/lab08.scm
+++
@@ -1,9 +0,0 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
-
-(define (composed f g) 'YOUR-CODE-HERE)
-
-(define (square n) (* n n))
-
-(define (pow base exp) 'YOUR-CODE-HERE)
-
-(define (ascending? lst) 'YOUR-CODE-HERE)
